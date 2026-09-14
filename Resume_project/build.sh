f30bb5fde9353711665e007c47b4c2eab7ba4173
AMAL-C-K <amalckg1999@gmail.com>
2026-09-15 00:26:31 +0530

    Update build.sh

--- Resume_project/build.sh
+++ Resume_project/build.sh
@@ -6,4 +6,10 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
+python manage.py createsuperuser \
+    --noinput \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL"
+
 python manage.py collectstatic --noinput
+
