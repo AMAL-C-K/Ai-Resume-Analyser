c575b8528edec348668e191468b272b1b91dadf6
AMAL-C-K <amalckg1999@gmail.com>
2026-09-14 23:49:11 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
 
         try {
             const response = await fetch(
-                "http://127.0.0.1:8000/api/login/",
+                "https://ai-resume-analyser-80vh.onrender.com/api/login/",
                 {
                     method: "POST",
 
