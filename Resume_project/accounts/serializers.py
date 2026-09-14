18ddebbf93f3087b91f4098d32abc8917a06fedc
AMAL-C-K <amalckg1999@gmail.com>
2026-09-15 00:44:01 +0530

    Update serializers.py

--- Resume_project/accounts/serializers.py
+++ Resume_project/accounts/serializers.py
@@ -10,6 +10,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = [ 'id', 'username', 'email', 'password',]
 
+    def validate_email(self, value):
+        if User.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError(
+                "An account with this email already exists."
+            )
+
+        return value
+
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data["username"],
